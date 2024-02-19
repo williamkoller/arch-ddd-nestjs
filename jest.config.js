9d44749816ae637490e771aa0a439f68d68321d9
William Koller <williamkoller30@gmail.com>
2024-02-19 16:38:23 -0300

    chore: finish setup

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main.ts',
+    '!<rootDir>/src/app.module.ts',
+    '!<rootDir>/src/infrastructure/**/*.module.ts',
+    '!<rootDir>/src/main/config/docs/*.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
